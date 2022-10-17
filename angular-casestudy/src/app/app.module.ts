2f0b6bfa42b5c744e3b191f74942c4b7abb17eb6
namhuynn1911 <namhuynh191193@gmail.com>
2022-10-17 16:12:32 +0700

    update

--- angular-casestudy/src/app/app.module.ts
+++ angular-casestudy/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ServiceListComponent } from './service-list/service-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    NavigationComponent,
+    ServiceListComponent
   ],
   imports: [
     BrowserModule
